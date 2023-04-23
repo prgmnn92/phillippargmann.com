874f4472d9cb7f7003200db0fe9c5b19dbfbb77b
Phillip Pargmann <pargmann92@gmail.com>
2023-04-23 10:08:18 +0200

    added new images

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -1,9 +1,9 @@
-import Head from 'next/head'
+import Head from "next/head"
 
-import { Card } from '@/components/Card'
-import { SimpleLayout } from '@/components/SimpleLayout'
-import { formatDate } from '@/lib/formatDate'
-import { getAllArticles } from '@/lib/getAllArticles'
+import { Card } from "@/components/Card"
+import { SimpleLayout } from "@/components/SimpleLayout"
+import { formatDate } from "@/lib/formatDate"
+import { getAllArticles } from "@/lib/getAllArticles"
 
 function Article({ article }) {
   return (
