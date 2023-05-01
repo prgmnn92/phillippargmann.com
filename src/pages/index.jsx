22ebf4b0c3a2f1a1ad003200430e8879fdb75347
Phillip Pargmann <pargmann92@gmail.com>
2023-05-01 11:46:37 +0200

    added netlify form

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -127,6 +127,7 @@ function Newsletter() {
           aria-label="Email address"
           required
           data-netlify="true"
+          netlify
           className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm"
         />
         <input type="hidden" name="form-name" value="newsletter-form" />
